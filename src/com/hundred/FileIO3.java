18e360f699393e4dd1ea5d95b15fc5b9a13c4ae5
wanghanjie <609470729@qq.com>
2020-12-27 13:43:40 +0800

    统计文件夹相同文件后缀名的个数

--- src/com/hundred/FileIO3.java
+++ src/com/hundred/FileIO3.java
@@ -1,28 +1,34 @@
 package com.hundred;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FileIO3 {
     public static void main(String[] args) {
         File file = new File("aaa");
-        String[] list = file.list();
-        Map<String,Integer> map = new HashMap();
-        for(String fileStr:list){
-            String[] split = fileStr.split("\\.");
-            String s = split[split.length - 1];
-            //寻找map里面是否有此后缀名的键，没有则创建
-            if(map.get(s)==null){
-                map.put(s,0);
+        Map<String,Integer> map = new HashMap<>();
+        Map map1 = countMap(map, file);
+        System.out.println(map1);
+    }
+    public static Map countMap(Map<String,Integer> map , File file){
+        File[] files = file.listFiles();
+        for(File sonfile:files){
+            if(sonfile.isFile()){
+                String name = sonfile.getName();
+                String[] split = name.split("\\.");
+                String s = split[split.length - 1];
+                if(!map.containsKey(s)){
+                    map.put(s,0);
+                }
+                Integer integer = map.get(s);
+                integer++;
+                map.put(s,integer);
+            }else{
+                countMap(map,sonfile);
             }
-            //判断此文件在map里面是那个键值对
-            Integer count = map.get(s);
-            //如果是计数+1
-            map.put(s,++count);
         }
-        for(Map.Entry<String,Integer> entry : map.entrySet()){
-            System.out.println(entry.getKey()+":"+entry.getValue());
-        }
+        return map;
     }
 }
--- src/com/hundred/test.java
+++ src/com/hundred/test.java
@@ -9,100 +9,11 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class test {
-    /**
-     *
-     * @param zipFile  压缩包文件对象
-     * @param listKey  压缩的图片物理地址
-     * @return
-     */
-    public static boolean packageZip(File zipFile, List<String> listKey){
-        //图片打包操作
-        ZipOutputStream zipStream = null;
-        FileInputStream zipSource = null;
-        BufferedInputStream bufferStream = null;
-        try {
-            zipStream = new ZipOutputStream(new FileOutputStream(zipFile));// 用这个构造最终压缩包的输出流
-//            zipSource = null;// 将源头文件格式化为输入流
-
-            for (String picKey : listKey) {
-
-                File file = new File(picKey);
-                System.out.println("uppic zipFile: " + picKey );
-                zipSource = new FileInputStream(file);
-
-                byte[] bufferArea = new byte[1024 * 10];// 读写缓冲区
-
-                // 压缩条目不是具体独立的文件，而是压缩包文件列表中的列表项，称为条目，就像索引一样
-                ZipEntry zipEntry = new ZipEntry(file.getName());
-                zipStream.putNextEntry(zipEntry);// 定位到该压缩条目位置，开始写入文件到压缩包中
-
-                bufferStream = new BufferedInputStream(zipSource, 1024 * 10);// 输入缓冲流
-                int read = 0;
-
-                // 在任何情况下，b[0] 到 b[off] 的元素以及 b[off+len] 到 b[b.length-1]
-                // 的元素都不会受到影响。这个是官方API给出的read方法说明，经典！
-                while ((read = bufferStream.read(bufferArea, 0, 1024 * 10)) != -1) {
-                    zipStream.write(bufferArea, 0, read);
-                }
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.err.println("zipStream下载文件报错："+ e);
-            return false;
-        } finally {
-            // 关闭流
-            try {
-                if (null != bufferStream)
-                    bufferStream.close();
-                if (null != zipStream)
-                    zipStream.close();
-                if (null != zipSource)
-                    zipSource.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                //e.printStackTrace();
-                System.err.println("close stream下载文件报错："+ e);
-                return false;
-            }
-        }
-        return true;
-    }
-
-//    public Boolean exportFile(List<String> fileNames, HttpServletResponse response, HttpServletRequest request) {
-//        QueryWrapper<查询数据> queryWrapper = new QueryWrapper();
-//        queryWrapper.in("file_name",fileNames);
-//        List<查询数据> cmtDealerSpotplanManagements = list(queryWrapper);
-//　　// 将数据处理
-//        List<String> spotPlanNames = cmtDealerSpotplanManagements.stream().map(查询数据::getFilePath).collect(Collectors.toList());
-//        List<ZipDto> list = new ArrayList<>();
-//　　// 数据为空则返回
-//        if ( CollectionUtils.isEmpty(spotPlanNames) ){
-//            return false;
-//        }
-//　　// 将数据地址去远程下载
-//        for (String fileUrl : spotPlanNames){
-//            File tempFile = new File(fileUrl);
-//            String newFileUrl = fileUrl.replace(" ", "%20");
-//            InputStream fileInputStream = fileService.download2(newFileUrl);
-//            String tempFileName = tempFile.getName();
-//            list.add(new ZipDto(tempFileName, fileInputStream));
-//        }
-//        String fileName = "test" + ".zip";
-//        ZipUtils.listStreamToZipStream(list, fileName, response);
-//        return true;
-//    }
-
     public static void main(String[] args) {
-        //test code
-        List<String> listKey = new ArrayList<String>();
-        listKey.add("D:/axis.log");
-        listKey.add("D:/iii.txt");
-
-        java.io.File zipFile = new java.io.File("D:/DownLoad4.zip");// 最终打包的压缩包
-        System.out.println("zipFile exists: " + zipFile.exists());
-        System.out.println("zipFile size: " + zipFile.length());
-        packageZip(zipFile,listKey);
-        System.out.println("zipFile exists2: " + zipFile.exists());
-        System.out.println("zipFile size: " + zipFile.length());
+        File file = new File("aaa");
+        boolean b = file.isFile();
+        File file2 = new File(file,"aaa.doc");
+        boolean b2 = file2.isFile();
+        System.out.println(b+","+b2);
     }
 }

f9cc4da43a2a1d4a4ae3a4a9a6ef83c5aa8ac95b
wanghanjie <609470729@qq.com>
2020-12-27 13:01:15 +0800

    统计文件夹相同文件后缀名的个数

--- src/com/hundred/FileIO3.java
+++ src/com/hundred/FileIO3.java
@@ -1,19 +1,28 @@
 package com.hundred;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FileIO3 {
     public static void main(String[] args) {
         File file = new File("aaa");
-        method1(file);
-    }
-
-    private static void method1(File file) {
         String[] list = file.list();
-        for (String str : list) {
-            String[] split = str.split("\\.");
+        Map<String,Integer> map = new HashMap();
+        for(String fileStr:list){
+            String[] split = fileStr.split("\\.");
             String s = split[split.length - 1];
-
+            //寻找map里面是否有此后缀名的键，没有则创建
+            if(map.get(s)==null){
+                map.put(s,0);
+            }
+            //判断此文件在map里面是那个键值对
+            Integer count = map.get(s);
+            //如果是计数+1
+            map.put(s,++count);
+        }
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey()+":"+entry.getValue());
         }
     }
 }

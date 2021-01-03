6edfd80681dde39edb16fb8e75a9b35efb7a8192
wanghanjie <609470729@qq.com>
2021-01-03 22:28:23 +0800

    输入流示例

---
+++ src/com/hundred/FileIO4.java
@@ -0,0 +1,19 @@
+package com.hundred;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FileIO4 {
+    public static void main(String[] args) throws IOException {
+        //1.输入流如果找不到对应的文件会自动报错
+        FileInputStream fis = new FileInputStream("aaa\\aaa.txt");
+        //2.定义一个变量来存储输入数据
+        int b = 0;
+        //3.输入数据如果为空，字节数为-1，所有判断为-1的就终止循环，停止输入
+        while((b = fis.read())!=-1){
+            System.out.println((char) b);
+        }
+        fis.close();
+    }
+}

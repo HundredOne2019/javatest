85b06a3d268b04d56e89d8a38d63ef8b9686f3e9
wanghanjie <609470729@qq.com>
2021-01-09 22:42:24 +0800

    对象序列化

---
+++ src/com/hundred/VO/User.java
@@ -0,0 +1,39 @@
+package com.hundred.VO;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 2L;
+    private String username;
+    private transient String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}

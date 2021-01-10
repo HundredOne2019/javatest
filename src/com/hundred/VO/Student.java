abbb90119e3060bea1d11d679dba56722006ae66
wanghanjie <609470729@qq.com>
2021-01-10 21:19:13 +0800

    Properties读写

---
+++ src/com/hundred/VO/Student.java
@@ -0,0 +1,48 @@
+package com.hundred.VO;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String studentName;
+    private String age;
+    private String sex;
+
+    public Student(String studentName, String age, String sex) {
+        this.studentName = studentName;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName='" + studentName + '\'' +
+                ", age='" + age + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}

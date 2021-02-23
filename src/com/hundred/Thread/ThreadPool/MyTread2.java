cd921b5089d61ccbb307d52c39afda1ab86af241
王汉杰 <609470729@qq.com>
2021-02-23 15:22:28 +0800

    线程池

--- src/com/hundred/Thread/ThreadPool/MyTread2.java
+++ src/com/hundred/Thread/ThreadPool/MyTread2.java
@@ -4,13 +4,16 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 默认线程池
+ * 最大线程池设定
  */
-public class MyThread {
+public class MyTread2 {
     public static void main(String[] args) {
-        //新建默认线程池newCachedThreadPool
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        //创建线程
+        //新建最大线程池newFixedThreadPool
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        //新建线程
+        executorService.submit(()->{
+            System.out.println(Thread.currentThread().getName());
+        });
         executorService.submit(()->{
             System.out.println(Thread.currentThread().getName());
         });
